21a2d37340412aed47980b38f6d55ac1dd0870ad
o_arnold <robert.arnold@medunigraz.at>
2024-02-01 11:00:13 +0100

    fixed CLI entry point

--- setup.py
+++ setup.py
@@ -72,45 +72,6 @@ except FileNotFoundError:
     long_description = DESCRIPTION
 
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        # self.status('Uploading the package to PyPI via Twine…')
-        # os.system('twine upload dist/*')
-        self.status('Uploading the package to TestPyPI via Twine…')
-        os.system('twine upload --repository testpypi dist/*')
-
-        # self.status('Pushing git tags…')
-        # os.system('git tag v{0}'.format(VERSION))
-        # os.system('git push --tags')
-
-        sys.exit()
-
-
 # Where the magic happens:
 setup(
     name=NAME,
@@ -127,7 +88,7 @@ setup(
     # py_modules=['pyceps'],
     entry_points={
         # command = package.module:function
-        'console_scripts': ['pyceps=pyceps.cli:run'],
+        'console_scripts': ['pyceps=src.cli:run'],
     },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
@@ -142,8 +103,4 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Development Status :: 4 - Beta',
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
 )
