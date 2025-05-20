3896032f40062d5599e37abd2d49766a7ad08fb6
o_arnold <robert.arnold@medunigraz.at>
2025-05-20 15:19:52 +0200

    fixed root path as string for ZIP archives

--- pyceps/fileio/pathtools.py
+++ pyceps/fileio/pathtools.py
@@ -488,7 +488,22 @@ class Repository:
         """
         study_root = self.root
         if isinstance(self.root, zipfile.Path):
-            study_root = os.path.abspath(self.root.root.filename)
+            if self.root.is_dir():
+                study_root = os.path.abspath(
+                    os.path.join(
+                        self.root.root.filename,
+                        self.root.at
+                    )
+                )
+            elif self.root.is_file():
+                study_root = os.path.abspath(
+                    os.path.dirname(
+                        os.path.join(
+                            self.root.root.filename,
+                            self.root.at
+                        )
+                    )
+                )
         if isinstance(self.root, py7zr.SevenZipFile):
             study_root = os.path.abspath(self.root.filename)
 
